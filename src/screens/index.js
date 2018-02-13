13dc8d6c693197ab104b2e95cd9cafe919e4cd6e
Cary <cary.wolff@swarmingtech.com>
2018-02-13 14:52:19 -0600

    fixed merge conflicts that got pushed

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Navigation } from 'react-native-navigation';
-<<<<<<< HEAD
 import { Provider } from 'react-redux'
 import store from '../redux/store'
-=======
-import { Provider } from 'react-redux';
-import { store } from '../redux/store';
->>>>>>> 97682ca1bd427c669739341f2dfa04bbb0f6d106
 
 import Login from '../../src/containers/Login';
 import Dashboard from '../../src/containers/Dashboard';
